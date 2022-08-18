12d08083be7d73c3f47c10e45269f198b99068db
Patil <Patil123@gmail.com>
2022-08-18 17:16:10 +0530

    patil modified file

--- NTHomePage.java
+++ NTHomePage.java
@@ -14,7 +14,8 @@ public class NTHomePage {
 	String url = "https://nichethyself.com/tourism/home.html";
 
 	public NTHomePage() {
-		System.out.println("");
+		System.out.println("i modified");
+		System.out.println("patil modified the file");
 		System.setProperty("webdriver.chrome.driver", 
 				"C:\\Users\\91820\\eclipse-workspace\\webdrivertraining\\test\\resources\\chromedriver.exe");
 		driver = new ChromeDriver();
